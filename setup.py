d827b1457f7a1a18da44565a3c2065a561a7bd86
Hassan Ibrar <118011584+Hassanibrar632@users.noreply.github.com>
2025-03-27 14:59:48 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ setup(
     description="This is an unofficial implementation of Real-ESRGAN and RealESRGANplus in the same module and the implementation process is simplified as much as it can be. Thank you.",
     author="M. Hassan Ibrar",
     url='https://github.com/Hassanibrar632/Real-ESRGAN',
-    packages=find_packages(include=['RealESRGAN', 'RealESRGANplus']),
+    packages=find_packages(include=['RealESRGAN', 'RealESRGANplus', 'RealESRGANplus.*']),  # Include submodules
     install_requires=[
         str(r)
         for r in pkg_resources.parse_requirements(
             open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
         )
-    ]
+    ],
 )
